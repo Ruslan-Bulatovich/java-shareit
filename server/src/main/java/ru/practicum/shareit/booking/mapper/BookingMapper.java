20db3f5721cf08161d94cb97b935d7c1ac215521
Ruslan-Bulatovich <ruslan.minulin.94@yandex.ru>
2023-05-29 16:39:59 +0300

    2

--- server/src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ server/src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingDtoResponse;
 import ru.practicum.shareit.booking.dto.BookingShortDto;
